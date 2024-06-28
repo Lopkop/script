c2344ec0a104616c1c8cd458ce7da4bcbc997dcb
Mark <84400463+Lopkop@users.noreply.github.com>
2024-06-28 10:35:42 +0000

    update main.py

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ import os
 from dotenv import load_dotenv
 
 from transactions import make_transactions
-from market import load_phantom, load_raydium
+from market import load_phantom, create_market
 
 load_dotenv()
 
 PRIVATE_KEY = os.getenv("PRIVATE_KEY")
+PUBLIC_KEY1 = os.getenv("PUBLIC_KEY1")
+PUBLIC_KEY2 = os.getenv("PUBLIC_KEY2")
+PUBLIC_KEY3 = os.getenv("PUBLIC_KEY3")
+BASE_COIN = os.getenv("BASE_COIN")
+QUOTE_TOKEN = os.getenv("QUOTE_TOKEN")
 
 if __name__ == '__main__':
-    make_transactions(PRIVATE_KEY)
+    # make_transactions(PRIVATE_KEY, PUBLIC_KEY1, PUBLIC_KEY2, PUBLIC_KEY3)
     load_phantom(PRIVATE_KEY)
-    load_raydium()
+    create_market(BASE_COIN, QUOTE_TOKEN)
+    # create_liquidity_pool()
+    # run_swap()
